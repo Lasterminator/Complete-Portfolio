553e535e1d9b9f0c02ec14485a7c1af5383a954a
Subhash Reddy <subhash8111999@gmail.com>
2024-02-15 21:16:45 -0500

    Update Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,10 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Pizzeria Management System',
-    subtitle: 'Database Management',
-    link: 'https://github.com/Lasterminator/CPSC-6620---DBMS',
-    image: '/images/projects/b.jpg',
-    date: '2023-05-01',
-    desc:
-    'Developed a Java-based pizza ordering system with full CRUD functionality for efficient data management.'
-    + 'Integrated and managed a robust database system for customer and order data handling.'
-    + 'Implemented stringent input validation and implemented methods to ensure data security against SQL injection.',
-  },
-
-  {
     title: 'Airbnb Price Determinants in Europe',
     subtitle: 'Data Analysis',
     link: 'https://github.com/Lasterminator/Math-8050---Data-Analysis/blob/main/Project/Project.pdf',
-    image: '/images/projects/h.png',
+    image: '/images/projects/datagraph.png',
     date: '2023-05-01',
     desc:
       'Conducted research on determinants of Airbnb prices in Europe using publicly available dataset "Airbnb Price Determinants" from Kaggle. '
@@ -25,10 +13,44 @@ const data = [
   },
   
   {
+    title: 'Pizzeria Management System',
+    subtitle: 'Database Management',
+    link: 'https://github.com/Lasterminator/CPSC-6620---DBMS',
+    image: '/images/projects/pizza.jpg',
+    date: '2023-05-01',
+    desc:
+    'Developed a Java-based pizza ordering system with full CRUD functionality for efficient data management.'
+    + 'Integrated and managed a robust database system for customer and order data handling.'
+    + 'Implemented stringent input validation and implemented methods to ensure data security against SQL injection.',
+  },
+
+  {
+    title: 'Professor-Course Review Portal',
+    subtitle: 'Web Development',
+    link: 'https://www.youtube.com/watch?v=jBJlrhmOhw4',
+    image: '/images/projects/coursereview.png',
+    date: '2023-05-01',
+    desc:
+      'Developed a comprehensive online platform for student evaluations of college professors, offering insights into teaching effectiveness and course structure. The portal facilitated anonymous feedback, allowing students to share honest reviews and ratings.' 
+      + 'Constructed the application using the MERN stack (MongoDB, Express.js, React.js, Node.js), ensuring a robust, scalable, and seamless user experience.',
+  },
+
+  {
+    title: 'Anonymous Biometrics-Based Authentication',
+    subtitle: 'Cryptography',
+    link: 'https://github.com/Lasterminator/CryptoProject/tree/main',
+    image: '/images/projects/crypto.png',
+    date: '2023-05-01',
+    desc:
+      'Implemented a Python-based cryptographic solution using Chebyshev polynomials and SHA-256 hash functions, inspired by the “Anonymous Biometrics-Based Authentication Scheme for Wireless Sensor Networks Using Chaotic Map” paper.' 
+      + 'Successfully implemented a secure authentication system which can be used for smart cards, Gateway Nodes, and sensor nodes, achieving 100% success in maintaining data confidentiality and integrity.',
+  },
+  
+  {
     title: 'Personal Portfolio',
     subtitle: 'Web Development',
     link: 'https://glowing-donut-151946.netlify.app',
-    image: '/images/projects/a.png',
+    image: '/images/projects/portfolio.png',
     date: '2023-05-01',
     desc:
       'Designed and implemented a Node.js-based personal website to display my portfolio, emphasizing my technical abilities and creativity.',
